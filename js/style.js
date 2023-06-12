60621b35dbd3e3246f62dddfef92a3c7bdd18f9e
devesh mishra <E22CSEU0766@bennett.edu.in>
2023-06-12 21:10:27 +0530

    style.js added interactivity to buttons.

---
+++ js/style.js
@@ -0,0 +1,21 @@
+var button = document.getElementById("myButton1");
+
+button.onclick = function() {
+  document.getElementById("men").scrollIntoView();
+};
+var button = document.getElementById("myButton2");
+
+button.onclick = function() {
+  document.getElementById("women").scrollIntoView();
+};
+var button = document.getElementById("myButton3");
+
+button.onclick = function() {
+  document.getElementById("kids").scrollIntoView();
+};
+var button = document.getElementById("myButton4");
+
+button.onclick = function() {
+  document.getElementById("Ocational").scrollIntoView();
+};
+
